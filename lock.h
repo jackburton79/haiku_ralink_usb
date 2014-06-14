ace8ba96f4e6f6a4e97c96530a8718a9128889cb
Stefano Ceccherini <stefano.ceccherini@gmail.com>
2014-06-14 13:27:00 +0200

    Use B_DO_NOT_RESCHEDULE in unlock()

--- lock.h
+++ lock.h
@@ -2,8 +2,8 @@
  * Copyright 2014 Stefano Ceccherini <stefano.ceccherini@gmail.com>
  * All rights reserved. Distributed under the terms of the MIT license.
  */
-#ifndef _H_LOCK
-#define _H_LOCK
+#ifndef _LOCK_H
+#define _LOCK_H_
 
 #include <OS.h>
 #include <KernelExport.h>
@@ -12,9 +12,8 @@
 typedef sem_id mutex;
 #define mutex_init(id, name) *id = create_sem(1, name)
 #define mutex_lock(id) acquire_sem(*id)
-#define mutex_unlock(id) release_sem(*id)
+#define mutex_unlock(id) release_sem_etc(*id, 1, B_DO_NOT_RESCHEDULE)
 #define mutex_destroy(id) delete_sem(*id)
-
 
 class MutexLocker {
 public:
